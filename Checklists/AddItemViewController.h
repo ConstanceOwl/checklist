f367cae3591b63ddd4ef4f34b45049265eea3f6c
Constance <lijiayi1987@126.com>
2015-02-27 18:08:50 +0800

    add addItenViewController
    addItenViewController

---
+++ Checklists/AddItemViewController.h
@@ -0,0 +1,16 @@
+//
+//  AddItemViewController.h
+//  Checklists
+//
+//  Created by Constance Li on 2/27/15.
+//  Copyright (c) 2015 GWrabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UITableViewController
+
+- (IBAction)cancel;
+- (IBAction)done;
+
+@end
